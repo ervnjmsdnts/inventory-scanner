569c788f230ff6dcc25166de4ad7e8ed1c7b1c30
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-11-27 18:07:00 +0800

    Fixed statistics

--- src/containers/Dashboard/Statistics.jsx
+++ src/containers/Dashboard/Statistics.jsx
@@ -15,20 +15,21 @@ const StatItem = ({ title, value = 0 }) => {
 }
 
 const Statistics = ({ orders }) => {
+  console.log({ orders })
   const today = moment()
   const todaysSalesValue = orders
     .filter(order => moment(order.createdAt).isSame(today, 'days'))
-    .reduce((acc, obj) => acc + obj.total, 0)
+    .reduce((acc, obj) => acc + obj.subTotal, 0)
 
   const past7Days = moment().subtract(7, 'days')
   const past7DaysValue = orders
     .filter(order => moment(order.createdAt) > past7Days)
-    .reduce((acc, obj) => acc + obj.total, 0)
+    .reduce((acc, obj) => acc + obj.subTotal, 0)
 
   const past30Days = moment().subtract(30, 'days')
   const past30DaysValue = orders
     .filter(order => moment(order.createdAt) > past30Days)
-    .reduce((acc, obj) => acc + obj.total, 0)
+    .reduce((acc, obj) => acc + obj.subTotal, 0)
 
   return (
     <Box width="100%" height="100%" border="1px solid gray" borderRadius="12px">
