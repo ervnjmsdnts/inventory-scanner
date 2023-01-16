1a3e301ebc9f79729d4011fd27bddc1e8d3a8b1b
czarinamaemalv <malvataanczarinamae@gmail.com>
2023-01-17 06:06:52 +0800

    CHanged api url

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://inventory-scanner-api.vercel.app/api'
+export const API_URL = 'https://store-api-bsu.vercel.app/'
 export const LOCAL_URL = 'http://localhost:8080/api'
 
 const client = axios.create({
